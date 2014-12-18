8f0147ad22f2e2972f16ff6f458f4aeefa31177f
Alper Kucukural <alper.kucukural@umassmed.edu>
2014-12-17 21:57:35 -0500

    Config info is reading from one location

--- public/php/config.php
+++ public/php/config.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 date_default_timezone_set('America/New_york');
+require_once("../../config/config.php");
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -12,11 +13,11 @@ date_default_timezone_set('America/New_york');
  */
 $sql_details = array(
 	"type" => "Mysql",  // Database type: "Mysql", "Postgres", "Sqlite" or "Sqlserver"
-	"user" => "biocore",       // Database user name
-	"pass" => "biocore2013",       // Database password
-	"host" => "localhost",       // Database host
+	"user" => DB_USER,       // Database user name
+	"pass" => DB_PASSWORD,       // Database password
+	"host" => DB_HOST,       // Database host
 	"port" => "",       // Database connection port (can be left empty for default)
-	"db"   => "biocore",       // Database name
+	"db"   => DB_NAME,       // Database name
 	"dsn"  => ""        // PHP DSN extra information. Set as `charset=utf8` if you are using MySQL
 );
 
