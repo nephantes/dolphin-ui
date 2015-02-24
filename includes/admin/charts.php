e3ae883de60b9857382f401163a91e141ca89fc3
Alper Kucukural <alper.kucukural@umassmed.edu>
2015-02-24 16:00:23 -0500

    Total runs removed

--- includes/admin/charts.php
+++ includes/admin/charts.php
@@ -3,11 +3,9 @@
                                 <!-- Tabs within a box -->
                                 <ul class="nav nav-tabs pull-right">
                                     <li class="active"><a href="#monthly-chart" data-toggle="tab">Dolphin Cluster Runs</a></li>
-                                    <li><a href="#total-chart" data-toggle="tab">Total Runs</a></li>
                                     <li class="pull-left header"><i class="fa fa-inbox"></i>Dolphin Jobs</li>
                                 </ul>
                                 <div class="tab-content no-padding">
-                                    <!-- Morris chart - Sales -->
                                     <div class="chart tab-pane active" id="monthly-chart" style="position: relative; height: 300px;"></div>
                                     <div class="chart tab-pane" id="total-chart" style="position: relative; height: 300px;"></div>
                                 </div>
