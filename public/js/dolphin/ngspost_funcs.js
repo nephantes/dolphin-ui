4184e3731fe8ce364451a73869dfe29cd6bbf1ba
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-05-14 11:39:29 -0400

    pipeline adds proper barcode code to ngs_runparams

--- public/js/dolphin/ngspost_funcs.js
+++ public/js/dolphin/ngspost_funcs.js
@@ -4,60 +4,67 @@
  *Ascription:
  */
 
- function postInsertRunparams(json, outputdir, name, description){
+function postInsertRunparams(json, outputdir, name, description){
 
-	var successCheck = false;
-	var runlistCheck = "";
-	var runID = "";
+   var successCheck = false;
+   var runlistCheck = "";
+   var runID = "";
+   var barcode = 1;
 
-    var uid = phpGrab.uid;
-    var gids = phpGrab.gids;
+   var uid = phpGrab.uid;
+   var gids = phpGrab.gids;
 
-	//check to make sure values are not empty
-	if (outputdir == "") {
-		outputdir = "/test/directory/change/me/";
-	}
-	if (name == "") {
-		name = "My Run";
-	}
-	if (description == "") {
-		description = "My Description";
-	}
+   //check to make sure values are not empty
+   if (outputdir == "") {
+       outputdir = "/test/directory/change/me/";
+   }
+   if (name == "") {
+       name = "My Run";
+   }
+   if (description == "") {
+       description = "My Description";
+   }
 
-	//find the run group ID
-	var hrefSplit = window.location.href.split("/");
-	var rerunLoc = $.inArray('rerun', hrefSplit)
-	var runGroupID;
-	if (rerunLoc != -1) {
-		runGroupID = hrefSplit[rerunLoc+1];
-	}else{
-		//if not a rerun
-		runGroupID = 'new';
-	}
+   //find the run group ID
+   var hrefSplit = window.location.href.split("/");
+   var rerunLoc = $.inArray('rerun', hrefSplit)
+   var runGroupID;
+   if (rerunLoc != -1) {
+       runGroupID = hrefSplit[rerunLoc+1];
+   }else{
+       //if not a rerun
+       runGroupID = 'new';
+   }
 
-	$.ajax({
-			type: 	'POST',
-			url: 	'/dolphin/public/ajax/ngsalterdb.php',
-			data:  	{ p: "submitPipeline", json: json, outdir: outputdir, name: name, desc: description, runGroupID: runGroupID, uid: uid, gids: gids},
-			async:	false,
-			success: function(r)
-			{
-				successCheck = true;
-				if (runGroupID == 'new') {
-					runlistCheck = 'insertRunlist';
-					runID = r;
-				}else{
-					runlistCheck = 'insertRunlist';
-					runID = (parseInt(runGroupID) + r);
-				}
-			}
-		});
-	if (successCheck) {
-		return [ runlistCheck, runID ];
-	}else{
-		return undefined;
-	}
- }
+   if (json.indexOf("\"barcodes\":\"none\"") != -1) {
+      barcode = 0;
+   }
+   
+   alert(json.indexOf("\"barcodes\":\"none\""));
+   
+   $.ajax({
+           type: 	'POST',
+           url: 	'/dolphin/public/ajax/ngsalterdb.php',
+           data:  	{ p: "submitPipeline", json: json, outdir: outputdir, name: name, desc: description, runGroupID: runGroupID, barcode: barcode, uid: uid, gids: gids},
+           async:	false,
+           success: function(r)
+           {
+               successCheck = true;
+               if (runGroupID == 'new') {
+                   runlistCheck = 'insertRunlist';
+                   runID = r;
+               }else{
+                   runlistCheck = 'insertRunlist';
+                   runID = (parseInt(runGroupID) + r);
+               }
+           }
+       });
+   if (successCheck) {
+       return [ runlistCheck, runID ];
+   }else{
+       return undefined;
+   }
+}
 
 function postInsertRunlist(runlistCheck, sample_ids, runID){
    var uid = phpGrab.uid;
