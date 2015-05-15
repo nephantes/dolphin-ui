ae4b11bb72840d743df5809daf42b7a526a377a5
Alper Kucukural <alper.kucukural@umassmed.edu>
2015-05-15 16:05:28 -0400

    workflow services and job apis added

---
+++ public/ajax/datajobout.php
@@ -0,0 +1,22 @@
+<?
+error_reporting(E_ALL);
+ini_set('report_errors','on');
+
+require_once("../../config/config.php");
+require_once("../../includes/dbfuncs.php");
+
+$id = $_GET['id'];
+
+$query = new dbfuncs();
+
+$data=$query->queryTable('
+SELECT j.jobname, jo.jobout FROM biocore.jobs j, biocore.jobsout jo
+where j.wkey=jo.wkey and j.job_num=jo.jobnum and j.job_id='.$id
+);
+
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+echo json_encode($data);
+exit;
