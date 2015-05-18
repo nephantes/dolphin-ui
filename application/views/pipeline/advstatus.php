5d9f26e9d11b5f506040a4f6630a489143c63a4b
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-05-18 13:47:01 -0400

    changed adv reports modal title

--- application/views/pipeline/advstatus.php
+++ application/views/pipeline/advstatus.php
@@ -3,7 +3,7 @@
 				  <div class="modal-content">
 					<div class="modal-header">
 					  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					  <h4 class="modal-title" id="myModalLabel">Delete Run</h4>
+					  <h4 class="modal-title" id="myModalLabel">Job Output</h4>
 					</div>
 					<form name="editForm" role="form" method="post">
 						<div class="modal-body">
