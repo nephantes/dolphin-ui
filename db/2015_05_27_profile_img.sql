63232d319e423c1bf86963c5663e156fe667fc60
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-07-22 15:21:30 -0400

    consolidated sql scripts into one

--- db/2015_05_27_profile_img.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `biocore`.`users` 
-ADD COLUMN `photo_loc` VARCHAR(255) NOT NULL DEFAULT '/public/img/avatar5.png' AFTER `lab`;
--- db/2015_07_16_organization.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `biocore`.`ngs_experiment_series` 
-ADD COLUMN `organization_id` INT(11) NULL DEFAULT NULL AFTER `lab_id`;
-
-ALTER TABLE `biocore`.`ngs_lab` 
-DROP COLUMN `organization_id`;
-
