fe9728bf5650527353d3c69266d1983d0b2785d6
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-07-24 15:40:03 -0400

    creates an excel file similar to import and fills some sample info

--- public/ajax/export_excel.php
+++ public/ajax/export_excel.php
@@ -8,6 +8,13 @@ require_once("../../includes/dbfuncs.php");
 require_once("../../includes/excel/Classes/PHPExcel.php");
 //header
 
+function num2alpha($n){
+		for($r = ""; $n >= 0; $n = intval($n / 26) - 1){
+			$r = chr($n%26 + 0x41) . $r;
+		}
+		return $r;
+}
+
 $query = new dbfuncs();
 
 if (isset($_GET['p'])){$p = $_GET['p'];}
@@ -23,6 +30,7 @@ if($p == 'exportExcel')
 	
 	//	Load in the empty excel template
 	$objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
+	$objPHPExcel->setActiveSheetIndex(3);
 	
 	//	Change values within the Excel file
 	if (isset($_GET['samples'])){$samples = $_GET['samples'];}
@@ -59,6 +67,52 @@ if($p == 'exportExcel')
 		LEFT JOIN ngs_treatment_manufacturer
 		ON ngs_samples.treatment_manufacturer_id = ngs_treatment_manufacturer.id
 		WHERE ngs_samples.id IN (".implode(",", $samples).")"));
+	
+	$experiment_series = $sample_data[0]->series_id;
+	$lane_ids = array();
+	$protocol_ids = array();
+	$col_number = 4;
+	
+	foreach($sample_data as $sd){
+		$objPHPExcel->getActiveSheet()->setCellValue('A'.$col_number, $sd->name);
+		//$objPHPExcel->getActiveSheet()->setCellValue('B'.$col_number, $sd->);		//	Lane name
+		//$objPHPExcel->getActiveSheet()->setCellValue('C'.$col_number, $sd->);		//	Protocol name
+		$objPHPExcel->getActiveSheet()->setCellValue('D'.$col_number, $sd->barcode);
+		$objPHPExcel->getActiveSheet()->setCellValue('E'.$col_number, $sd->title);
+		$objPHPExcel->getActiveSheet()->setCellValue('F'.$col_number, $sd->batch_id);
+		$objPHPExcel->getActiveSheet()->setCellValue('G'.$col_number, $sd->source_symbol);
+		$objPHPExcel->getActiveSheet()->setCellValue('H'.$col_number, $sd->source);
+		$objPHPExcel->getActiveSheet()->setCellValue('I'.$col_number, $sd->organism);
+		$objPHPExcel->getActiveSheet()->setCellValue('J'.$col_number, $sd->biosample_type);
+		$objPHPExcel->getActiveSheet()->setCellValue('K'.$col_number, $sd->molecule);
+		$objPHPExcel->getActiveSheet()->setCellValue('L'.$col_number, $sd->description);
+		$objPHPExcel->getActiveSheet()->setCellValue('M'.$col_number, $sd->instrument_model);
+		$objPHPExcel->getActiveSheet()->setCellValue('N'.$col_number, $sd->avg_insert_size);
+		$objPHPExcel->getActiveSheet()->setCellValue('O'.$col_number, $sd->read_length);
+		$objPHPExcel->getActiveSheet()->setCellValue('P'.$col_number, $sd->genotype);
+		//$objPHPExcel->getActiveSheet()->setCellValue('Q'.$col_number, $sd->);		//	condition symbols
+		//$objPHPExcel->getActiveSheet()->setCellValue('R'.$col_number, $sd->);		//	conditions
+		$objPHPExcel->getActiveSheet()->setCellValue('S'.$col_number, $sd->concentration);
+		$objPHPExcel->getActiveSheet()->setCellValue('T'.$col_number, $sd->treatment_manufacturer);
+		$objPHPExcel->getActiveSheet()->setCellValue('U'.$col_number, $sd->donor);
+		$objPHPExcel->getActiveSheet()->setCellValue('V'.$col_number, $sd->time);
+		$objPHPExcel->getActiveSheet()->setCellValue('W'.$col_number, $sd->biological_replica);
+		$objPHPExcel->getActiveSheet()->setCellValue('X'.$col_number, $sd->technical_replica);
+		$objPHPExcel->getActiveSheet()->setCellValue('Y'.$col_number, $sd->spike_ins);
+		$objPHPExcel->getActiveSheet()->setCellValue('Z'.$col_number, $sd->adapter);
+		$objPHPExcel->getActiveSheet()->setCellValue('AA'.$col_number, $sd->notebook_ref);
+		$objPHPExcel->getActiveSheet()->setCellValue('AB'.$col_number, $sd->notes);
+		
+		//	Push lane_ids and protocol_ids
+		if(!in_array($sd->lane_id, $lane_ids)){
+			array_push($lane_ids, $sd->lane_id);
+		}
+		if(!in_array($sd->protocol_id, $protocol_ids)){
+			array_push($protocol_ids, $sd->protocol_id);
+		}
+		
+		$col_number++;
+	}
 	
 	//	Save the file to be downloaded
 	$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
