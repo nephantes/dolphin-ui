1e3e6e116681d9b147ed0f1726ef29e702f6b5d8
Nick Merowsky <nick.merowsky@umassmed.edu>
2015-11-12 17:05:30 +0000

    database check to make sure user is registered

--- includes/login.php
+++ includes/login.php
@@ -4,79 +4,78 @@ error_reporting(E_ERROR);
 ini_set('report_errors','on');
 
 
-function checkLDAP($username, $password)
-{
+function checkLDAP($username, $password){
   $ldapserver = 'edunivad02.ad.umassmed.edu';
   $dn_string  = 'ou=Accounts,dc=ad,dc=umassmed,dc=edu';
   $binduser   = 'SVCLinuxLDAPAuth';
   $bindpass   = 'Umass2008';
   try{
- 
-  $connection = ldap_connect($ldapserver);
-  ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
-  ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
- 
-  if($connection)
-  {
-     $bind = ldap_bind($connection, $binduser, $bindpass );
-     if($bind)
-     {
-             $filter="sAMAccountName=".$username."*";
-             $result = ldap_search($connection,$dn_string,$filter) or die ("Search error.");
-             $data = ldap_get_entries($connection, $result);
-             $binddn = $data[0]["dn"];
-             if (!isset($binddn))
-               return 0;
-             $bind = ldap_bind($connection, $binddn, $password);
-             if($bind) 
-                return 1;
-             else
-                return 0;
-      }
-     else
-     {
-	return 0;
-     }
-  }
-  }catch (Exception $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
+	$connection = ldap_connect($ldapserver);
+	ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+	ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
+   
+	if($connection){
+	  $bind = ldap_bind($connection, $binduser, $bindpass );
+	  if($bind){
+		$filter="sAMAccountName=".$username."*";
+		$result = ldap_search($connection,$dn_string,$filter) or die ("Search error.");
+		$data = ldap_get_entries($connection, $result);
+		$binddn = $data[0]["dn"];
+		if (!isset($binddn))
+		  return 0;
+		$bind = ldap_bind($connection, $binddn, $password);
+		if($bind) 
+		  return 1;
+		else
+		  return 0;
+	  }else{
+		return 0;
+	  }
+	}
+  }catch (Exception $e){
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 	return 0;
   }
 }
 
 if(!empty($_POST) && isset($_POST['password'])){ 
-        $login_ok = false; 
-        $post_pass=hash('md5', $_POST['password'] . "12as7ad8s9d9a0") . hash('sha256', $_POST['password'] . "1m2kmk211kl123k");
+  $login_ok = false; 
+  $post_pass=hash('md5', $_POST['password'] . "12as7ad8s9d9a0") . hash('sha256', $_POST['password'] . "1m2kmk211kl123k");
 
-        if ($post_pass == "09e59212d1195ec28d207a1243b9c76c0e57bfd50f5b5fcfb5fb887298aabef49a3c0e878c593a0ab056a364927f6ce0"){
-          $res=1;
-        }
-        else{
-          $res=checkLDAP($_POST['username'], $_POST['password']);
-        }
-        #$res=1;
-        $e=$res;
-        if($res==1){
-            $login_ok = true;
-        } 
- 
-        if($login_ok){ 
-            $s="Succefull";
-            $_SESSION['user'] = $_POST['username'];
-            require_once("dbfuncs.php");
-            $query=new dbfuncs();
-            $_SESSION['uid'] = $query->getUserID($_SESSION['user']);
+  if ($post_pass == "09e59212d1195ec28d207a1243b9c76c0e57bfd50f5b5fcfb5fb887298aabef49a3c0e878c593a0ab056a364927f6ce0"){
+	$res=1;
+  }
+  else{
+	$res=checkLDAP($_POST['username'], $_POST['password']);
+  }
+  #$res=1;
+  $e=$res;
+  if($res==1){
+	$login_ok = true;
+  } 
 
-            if ($_POST['username'] == "kucukura" || $_POST['username']=="garberm")
-            {
-              $_SESSION['admin'] = "admin";
-            }
-        } 
-        else{ 
-  	    session_destroy();
-            require_once("../includes/loginform.php");
-            exit;
-            $e="Login Failed."; 
-        } 
+  if($login_ok){ 
+	$s="Succefull";
+	$_SESSION['user'] = $_POST['username'];
+	require_once("dbfuncs.php");
+	$query=new dbfuncs();
+	$uid_check = $query->getUserID($_SESSION['user']);
+	if($uid_check != "0"){
+	  $_SESSION['uid'] = $uid_check;
+	  if ($_POST['username'] == "kucukura" || $_POST['username']=="garberm"){
+		$_SESSION['admin'] = "admin";
+	  }
+	}else{
+	  session_destroy();
+	  require_once("../includes/loginform.php");
+	  exit;
+	  $e="Login Failed.";
+	}
+  }else{ 
+	session_destroy();
+	require_once("../includes/loginform.php");
+	exit;
+	$e="Login Failed.";
+  } 
 } 
 ?>
