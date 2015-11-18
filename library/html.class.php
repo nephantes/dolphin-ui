ec8eb4051445dc0ec1a7e6060957502ef609ca83
Nick Merowsky <nick.merowsky@umassmed.edu>
2015-11-18 14:08:49 +0000

    submit button change

--- library/html.class.php
+++ library/html.class.php
@@ -581,15 +581,27 @@ e range"><i class="fa fa-calendar"></i></button>
 	function getSubmitBrowserButton()
 	{
 	$html = '';
-	$html.= '<div id="btn-group">';
-	$html.= '	<input type="button" class="btn btn-primary" name="pipeline_button" value="Send to Pipeline" onClick="submitSelected();"/>
-				<input type="button" class="btn btn-primary" name="send_to_status_button" value="Pipeline Status" onClick="sendToStatus()"/>
-				<input type="button" class="btn btn-primary" name="export_excel_button" value="Export to Excel" onClick="exportExcel()"/>
-				<input type="button" class="btn btn-primary" name="table_generation_button" value="Generate Tables" onClick="generateTableLink()"/>
-				<input type="button" class="btn btn-primary" name="send_to_NCBI_button" value="Send to NCBI" onClick="sendToStatus()" disabled/>
-				<input type="button" class="btn btn-primary" name="send_to_ENCODE_button" value="Send to ENCODE" onClick="sendToStatus()" disabled/>
-				<input type="button" class="btn btn-danger pull-right" name="delete_Selected" value="Delete Selected" onclick="deleteButton('.$_SESSION['uid'].')"/>';
-	$html.= '</div>';
+	$html.= '<div class="col-md-9">
+				<div class="margin pull-left btn-group">
+					<button id="dso_menu" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Data Selection Options <span class="fa fa-caret-down"></span></button>
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dso_menu" onClick="scrollWindow(0,200)">
+						<li><a name="pipeline_button">Send to Pipeline</a></li>
+						<li><a name="send_to_status_button" onClick="sendToStatus()">Pipeline Status</a></li>
+						<li><a name="export_excel_button" onClick="exportExcel()">Export to Excel</a></li>
+						<li class="divider"></li>
+						<li><a name="table_generation_button" onClick="generateTableLink()">Generate Tables</a></li>
+						<li class="divider"></li>
+						<li><a name="change_group_perms" onClick="changeDataGroup()" disabled>Change Data Group</a></li>';
+	if($_SESSION['uid'] == 1){
+							#<li><a name="send_to_NCBI_button" onClick="" disabled>Send to NCBI</a></li>
+		$html.= 			'<li><a name="send_to_ENCODE_button" onClick="checkForEncodeSubmission()" disabled>Send to ENCODE</a></li>';
+	}
+	$html.= 		'</ul>';
+	$html.= '	</div>
+				<div class="margin pull-right">
+					<input type="button" class="btn btn-danger" name="delete_Selected" value="Delete Selected" onclick="deleteButton('.$_SESSION['uid'].')"/>
+				</div>
+			</div>';
 	return $html;
 	}
 	function getSelectionBox($title, $selection){
