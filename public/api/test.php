66b0dfb52f978eedde1c4440c40f58531c825151
Alper Kucukural <alper.kucukural@umassmed.edu>
2015-11-18 20:15:24 -0500

    CheckFile and Permission in the cluster functions added to API

--- public/api/test.php
+++ public/api/test.php
@@ -7,9 +7,12 @@ error_reporting(E_ALL);
 ini_set('report_errors','on');
 
 $myClass = new funcs();
-$result=$myClass->getINI();
-$result=json_encode($result);
-#$data = $result;
+
+$result=$myClass->startService("step1", "khAO8pFxKzigjtrMyk1M660xKOG6j", "ls -l");
+
+#$result=$myClass->getINI();
+#$result=json_encode($result);
+$data = $result;
 
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
