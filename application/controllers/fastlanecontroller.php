38eac7d93e5f7e6205cad247283c0c5de2562a25
Nick Merowsky <nick.merowsky@umassmed.edu>
2015-11-24 02:27:26 +0000

    bolder error statements

--- application/controllers/fastlanecontroller.php
+++ application/controllers/fastlanecontroller.php
@@ -61,12 +61,14 @@ class FastlaneController extends VanillaController {
 					}else if($key == 4){
 						$text.="Experiment field is either empty or contains improper white space<br>";
 					}else if($key == 5){
+						$text.="<h3>Input Directory</h3>";
 						$text.="Input Directory is either empty, contains improper white space, or you do not have permissions to access it:<br>";
 						$text.="<font color=\"red\">".$fastlane_values[$key]."</font><br><br>";
 						$text.="If you're not sure if you have cluster access, visit <a href='http://umassmed.edu/biocore/resources/galaxy-group/'>this website</a> for more help.<br><br>";
 						$text.="If you're already within the cluster, run this script to give dolphin access to your user account to run on your behalf:<br>";
 						$text.="/project/umw_biocore/bin/addKey.bash <br><br>";
 					}else if($key == 6){
+						$text.="<h3>Files</h3>";
 						$text.="There was an error with the file information:<br>";
 						if(count($bad_files_array) > 0){
 							foreach($bad_files_array as $bfa){
@@ -76,7 +78,8 @@ class FastlaneController extends VanillaController {
 							$text.="<font color=\"red\">The files listed are not in the proper fastlane format.</font><br><br>";
 						}
 					}else if($key == 7){
-						$text.="Backup directory is either empty, contains improper white space, or you do not have permissions to access it:<br><br>";
+						$text.="<h3>Process Directory</h3>";
+						$text.="Process directory is either empty, contains improper white space, or you do not have permissions to access it:<br><br>";
 						$text.="<font color=\"red\">".$fastlane_values[$key]."</font><br><br>";
 						$text.="If you're not sure if you have cluster access, visit <a href='http://umassmed.edu/biocore/resources/galaxy-group/'>this website</a> for more help.<br><br>";
 						$text.="If you're already within the cluster, run this script to give dolphin access to your user account to run on your behalf:<br>";
