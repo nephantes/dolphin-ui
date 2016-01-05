ec8c02208cf2b18f3ce10e7ceea3838312580db9
Nick Merowsky <nick.merowsky@umassmed.edu>
2016-01-05 22:10:11 +0000

    Revert "readthedocs now sends to correct location"
    This reverts commit fa251c1c0bfee87f78e6805bb3c89d970c0150c0.

--- includes/tasks.php
+++ includes/tasks.php
@@ -1,6 +1,6 @@
 <!-- Tasks: style can be found in dropdown.less -->
                         <li class="dropdown tasks-menu">
-                            <a href="http://dolphin.readthedocs.org">
+                            <a href="http://dolphin.readthedocs.org/en/develop/index.html">
                                 <i class="fa fa-mortar-board"></i>
                                 <span class="label label-danger"></span>
                             </a>
