2d22dabfea4ecfcedea8f1205895c2bbaea28ff9
Nick Merowsky <nick.merowsky@umassmed.edu>
2016-01-28 14:17:34 +0000

    headers testing

--- public/ajax/profiledb.php
+++ public/ajax/profiledb.php
@@ -239,9 +239,8 @@ else if ($p == 'getUID')
 
 
 
-#header('Cache-Control: no-cache, must-revalidate');
-#header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-#header('Content-type: application/json');
+header('Cache-Control: no-cache, must-revalidate');
+header('Content-type: application/json');
 echo $data;
-#exit;
+exit;
 ?>
