ed20d5b39df57d5499798d429009de9076c590ff
Rhaknam <alper.kucukural@umassmed.edu>
2016-01-27 22:31:00 -0500

    test

--- public/ajax/adminstatquerydb.php
+++ public/ajax/adminstatquerydb.php
@@ -105,9 +105,9 @@ else if($p == "getJobTime")
     ");
 }
 
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Content-type: application/json');
+#header('Cache-Control: no-cache, must-revalidate');
+#header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+#header('Content-type: application/json');
 echo $data;
-exit;
+#exit;
 ?>
