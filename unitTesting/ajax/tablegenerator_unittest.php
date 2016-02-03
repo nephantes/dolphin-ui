4d0fdaa27a8734e1387feb8d5ca26d2ab82d71e3
Rhaknam <alper.kucukural@umassmed.edu>
2016-02-02 21:10:22 -0500

    test push

--- unitTesting/ajax/tablegenerator_unittest.php
+++ unitTesting/ajax/tablegenerator_unittest.php
@@ -51,7 +51,7 @@ class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 	public function testCreateTableFile(){
 		ob_start();
 		$_GET['p'] = 'createTableFile';
-		$_GET['url'] = substr(getcwd(), 0, strlen(getcwd())) . 'getsamplevals.php';
+		$_GET['url'] = substr(getcwd(), 0, strlen(getcwd()) - 11) . 'public/api/getsamplevals.php';
 		$_GET['samples'] = 'samples=1,2,3,4,5,6:3';
 		$_GET['file'] = 'file=rsem/genes_expression_tpm.tsv';
 		$_GET['common'] = 'common=gene,transcript';
@@ -115,7 +115,7 @@ class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 	public function testConvertToTSV(){
 		ob_start();
 		$_GET['p'] = 'convertToTSV';
-		$_GET['url'] = substr(getcwd(), 0, count(getcwd()) -  . 'getsamplevals.php';
+		$_GET['url'] = substr(getcwd(), 0, strlen(getcwd()) - 11) . 'public/api/getsamplevals.php';
 		$_GET['samples'] = 'samples=1,2,3,4,5,6:3';
 		$_GET['file'] = 'file=rsem/genes_expression_tpm.tsv';
 		$_GET['common'] = 'common=gene,transcript';
