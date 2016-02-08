87e50f5d57588c5f418e7fcb72f91d16ca10b8bf
Nick Merowsky <nick.merowsky@umassmed.edu>
2016-02-08 20:28:32 +0000

    testing email

--- scripts/dolphin_wrapper.py
+++ scripts/dolphin_wrapper.py
@@ -569,7 +569,6 @@ class Dolphin:
         sender = 'biocore@umassmed.edu'
         if (email_type == '3'):
             receiver = 'biocore@umassmed.edu'
-            receiver = email_check[0][1]
             subject = 'There has been an error in run: %s' % run_id
             body = 'Run %s has ended with an error' % run_id;
         if (email_type == '1' and email_check[0][2] == 1):
@@ -684,11 +683,11 @@ def main():
               p.stdout.flush()
               if (re.search('failed\n', line) or re.search('Err\n', line) ):
                  logging.info("failed")
-                 dolphin.send_email('3', 'merowskn', '127.0.0.1:25', runparamsids[0][0]);
+                 dolphin.send_email('3', runparamsids[0][1], '127.0.0.1:25', runparamsids[0][0]);
                  dolphin.stop_err("failed")
         dolphin.send_email('1', 'merowskn', '127.0.0.1:25', runparamsids[0][0]);
    except Exception, ex:
-        dolphin.send_email('3', 'merowskn', '127.0.0.1:25', runparamsids[0][0]);
+        dolphin.send_email('3', runparamsids[0][1], '127.0.0.1:25', runparamsids[0][0]);
         dolphin.stop_err('Error (line:%s)running dolphin_wrapper.py\n%s'%(format(sys.exc_info()[-1].tb_lineno), str(ex)))
 
    sys.exit(0)
