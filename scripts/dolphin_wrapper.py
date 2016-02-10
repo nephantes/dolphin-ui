24d96bdf9671e0c6dcd348c4356dbb34cedb4914
Nick Merowsky <nick.merowsky@umassmed.edu>
2016-02-10 23:52:08 +0000

    email

--- scripts/dolphin_wrapper.py
+++ scripts/dolphin_wrapper.py
@@ -561,18 +561,18 @@ class Dolphin:
         sys.exit(2)
         
     # email
-    def send_email(self, run_result, username, run_id):
+    def send_email(self, username, run_id):
         email_sender=dolphin.config.get(self.dolphin.params_section, "email_sender")
         email_err_receiver=dolphin.config.get(self.dolphin.params_section, "email_err_receiver")
         run_sql = "SELECT run_status FROM ngs_runparams where id = %s;"%run_id
         end_email_check=self.runSQL(run_sql%locals())
         user_sql = "SELECT name, email, email_toggle FROM users where username = '%s';"%username
         email_check=self.runSQL(user_sql%locals())
-        if (run_result == '1' and email_check[0][2] == 1):
+        if (end_email_check[0][0] == '1' and email_check[0][2] == 1):
             receiver = email_check[0][1]
             subject = 'Your Dolphin run has completed!'
             body = 'Your Dolphin run #%s has completed successfully!' % run_id;
-        if (run_result == '3'):
+        if (end_email_check[0][0] == '3'):
             receiver = email_err_receiver
             subject = 'There has been an error in run: %s' % run_id
             body = 'Run %s has ended with an error' % run_id;
@@ -687,7 +687,7 @@ def main():
                  dolphin.send_email(end_email_check[0][0], runparamsids[0][1], runparamsids[0][0]);
                  dolphin.stop_err("failed")
         #Send email when finished
-        dolphin.send_email(end_email_check[0][0], runparamsids[0][1], runparamsids[0][0]);
+        dolphin.send_email(runparamsids[0][1], runparamsids[0][0]);
    except Exception, ex:
         dolphin.stop_err('Error (line:%s)running dolphin_wrapper.py\n%s'%(format(sys.exc_info()[-1].tb_lineno), str(ex)))
 
