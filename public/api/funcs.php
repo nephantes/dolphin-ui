13bcb8a8c03060b225b9a5d73c5355961f2909cb
Alper Kucukural <alper.kucukural@umassmed.edu>
2016-02-10 22:03:14 -0500

    jobnum typo fix

--- public/api/funcs.php
+++ public/api/funcs.php
@@ -597,7 +597,7 @@ class funcs
         $service_id  = $this->getId("service", $username, $servicename, $wkey, "");
         if ($field=="end_time")
         {
-               $this->checkStartTime($wkey, $job_num, $username);
+               $this->checkStartTime($wkey, $jobnum, $username);
         }
         
         $sql = "update jobs set `$field`=now(), `result`='$result' where `wkey`='$wkey' and `job_num`='$jobnum'";
