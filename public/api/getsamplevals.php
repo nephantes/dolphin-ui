9844320b54d30174ed697f7e91bd6450113d0ed1
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-03-16 13:10:05 -0400

    table loading

--- public/api/getsamplevals.php
+++ public/api/getsamplevals.php
@@ -65,7 +65,6 @@ foreach ($a as $i => $row){
     }
     $url=API_PATH."/public/api/?source=".API_PATH."/public/pub/".$i."/$file&$fields&format=json";
     $json = file_get_contents($url);
-    var_dump($json);
     $dat1 = json_decode($json, $array=TRUE);
     foreach ($dat1[0] as $key => $value){
         if (!in_array($key, $title) || in_array($key, $keepcols_array)){
