ed487f6c062bf895c65489f1508953abaa71b381
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-03-16 16:33:04 -0400

    index testing

--- db/2016_03_16_debrowser_link.sql
+++ db/2016_03_16_debrowser_link.sql
@@ -1,2 +1,2 @@
 INSERT INTO `sidebar` (`id`, `name`, `parent_name`, `link`, `owner_id`, `group_id`, `perms`, `date_created`, `date_modified`, `last_modified_user`)
-VALUES ('15', 'DEBrowser', 'NGS Tracking', 'debrowser', '1', '1', '63', NOW(), NOW(), '1');
+VALUES ('15', 'DEBrowser', 'NGS Tracking', 'debrowser/index/index', '1', '1', '63', NOW(), NOW(), '1');
