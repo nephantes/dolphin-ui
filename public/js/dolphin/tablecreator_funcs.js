c0833115412683b018d22a8e348e9da62942f6bd
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-03-16 16:01:09 -0400

    testing debrowser json file loading

--- public/js/dolphin/tablecreator_funcs.js
+++ public/js/dolphin/tablecreator_funcs.js
@@ -620,9 +620,14 @@ $(function() {
 		var json_obj = '';
 		var export_table = document.getElementById('downloadOptions');
 		var debrowser_string = '';
-		if (beforeFormat.indexOf('rsem/') > -1) {
-			debrowser_string = '<li class="divider"></li>' +
-				'<li><a onclick="sendTableToDebrowser(\''+BASE_PATH+'/public/api/getsamplevals.php?'+beforeFormat+'format=json\')" style="cursor:pointer">Send to DEBrowser</a></li>';
+		if (beforeFormat.indexOf('rsem/') > -1 || beforeFormat.indexOf('mRNA') > -1 || beforeFormat.indexOf('tRNA') > -1) {
+			if (table_params.file != null && table_params.file != '') {
+				debrowser_string = '<li class="divider"></li>' +
+					'<li><a onclick="sendTableToDebrowser(\''+BASE_PATH+'/public/tmp/files/'+table_params.file+'\')" style="cursor:pointer">Send to DEBrowser</a></li>';
+			}else{
+				debrowser_string = '<li class="divider"></li>' +
+					'<li><a onclick="sendTableToDebrowser(\''+BASE_PATH+'/public/api/getsamplevals.php?'+beforeFormat+'format=json\')" style="cursor:pointer">Send to DEBrowser</a></li>';
+			}
 		}
 		
 		var ul = createElement('ul', ['class','role'],['dropdown-menu','menu']);
@@ -667,7 +672,7 @@ $(function() {
 						}
 						var debrowser_string = '';
 						var datafile = splitParameters[1].split('file=')[1].split(',').join(', ');
-						if (datafile.indexOf('rsem') > -1 || datafile.indexOf('mRNA') > -1 || datafile.indexOf('tRNA')) {
+						if (datafile.indexOf('rsem') > -1 || datafile.indexOf('mRNA') > -1 || datafile.indexOf('tRNA') > -1) {
 							if (s[x].file != null && s[x].file != '') {
 								debrowser_string = '<li><a id="' + s[x].id+'" onclick="sendTableToDebrowser(\''+BASE_PATH+'/public/tmp/files/'+s[x].file+'\')">Send to DEBrowser</a></li>' +
 									'<li class="divider"></li>';
