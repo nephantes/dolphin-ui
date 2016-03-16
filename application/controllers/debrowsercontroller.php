f6972d155e1358cedded3cf368c57cd32b2b6bdb
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-03-16 14:04:16 -0400

    debrowser loading

--- application/controllers/debrowsercontroller.php
+++ application/controllers/debrowsercontroller.php
@@ -9,7 +9,7 @@ class DebrowserController extends VanillaController {
 	function index() {
 		$this->set('title','DEBrowser');
 		if(isset($_SESSION['debrowser'])){
-			$this->set('jsonobject', '/?jsonobject='.urlencode($_SESSION['debrowser']));
+			$this->set('jsonobject', '/?jsonobject='.urlencode($_SESSION['debrowser']).'&title=no');
 		}else if ($_SESSION['debrowser'] == ''){
 			$this->set('jsonobject','/?title=no');
 		}else{
