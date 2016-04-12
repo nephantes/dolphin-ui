98457118722b4367b3234171e3d461ac0bf6f78a
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-04-12 18:08:52 +0000

    html5shiv and respond

--- application/views/auths/header.php
+++ application/views/auths/header.php
@@ -12,8 +12,8 @@
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <script src="<?php echo BASE_PATH?>/css/html5shiv/3.7.0/html5shiv.min.js"></script>
+		<script src="<?php echo BASE_PATH?>/css/respond/1.3.0/respond.min.js"></script>
         <![endif]-->
         
         <?php echo $html->getBasePath(BASE_PATH, API_PATH); ?> 
