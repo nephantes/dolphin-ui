e61ef366d4c1c4cc5316053da0c5e4ee841d3ec5
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-04-12 17:33:06 +0000

    awesome font added

--- application/views/auths/header.php
+++ application/views/auths/header.php
@@ -5,7 +5,7 @@
         <title>Biocore - Dolphin | Log in</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo BASE_PATH?>/css/font-awesome-4.6.1/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
         <link href="css1/AdminLTE.css" rel="stylesheet" type="text/css" />
 
