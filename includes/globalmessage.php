e215c2ccd5d8171e80e7523b87daffbfee512cc3
Nicholas Merowsky <nmerowsky@gmail.com>
2016-10-25 17:22:23 -0400

    Update globalmessage.php (#546)

--- includes/globalmessage.php
+++ includes/globalmessage.php
@@ -1,12 +1,4 @@
 <div class="row">
 	<div class="col-md-12">
-		<?php
-		'''
-		<div class="callout callout-danger lead">
-			<h4>The GHPC Cluster will be down for the weekend of Oct. 21st - Oct. 23rd.<br><br>
-				Please refrain from creating or importing any runs while the cluster is offline.</h4>
-		</div>
-		'''
-		?>
 	</div>
 </div>
