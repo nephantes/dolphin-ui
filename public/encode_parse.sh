4ab3e73b569c0f03ca32bb553a17b736b6753421
Alper Kucukural <alper.kucukural@umassmed.edu>
2017-01-25 18:02:37 -0500

    mysql salt bug fix

---
+++ public/encode_parse.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat pub/DSAFtfb6flbk1WZt8J204eV6dlYLlY/summary/summary.tsv pub/iCj0OhptA7iBoy8ERYC9EKomCgFElI/summary/summary.tsv pub/eQiaoRszBNn6CkK3H2K0TOdjG2bXM3/summary/summary.tsv pub/oZsHflfUTUPPINpzVp7qrqYMggGdxM/summary/summary.tsv pub/BbvPYk0t1HYXLCAeb0Lc85DVNPFZW1/summary/summary.tsv | grep -v 'Total ' ;
