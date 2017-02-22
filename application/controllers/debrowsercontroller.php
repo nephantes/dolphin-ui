5389fd258f9c2e8a90b1de1e5a81e2daa6df5b49
Alper Kucukural <alper.kucukural@umassmed.edu>
2017-02-22 11:50:51 -0500

    2017 02 22 haplotype chip atac check fix (#596)
    * Dolphin run count fix
    
    * keys added for md5sum checks on amazon. Crypt/decrypt funcionality for
    amazon keys started being developed
    
    * tempororay fix for error
    
    * date check added for amazon backup controls
    
    * crypt deleted
    
    * favicon location changed
    
    * Custom index filter out fix
    
    * fixed custom mapping
    
    * fixed custom mapping
    
    * table fix and infobox update
    
    * trim quality order fix
    
    * chip parameter fix
    
    * owner added for backup queries on amazon
    
    * haplotype caller atac chip seq check fix

--- application/controllers/debrowsercontroller.php
+++ application/controllers/debrowsercontroller.php
@@ -8,14 +8,10 @@ class DebrowserController extends VanillaController {
 	
 	function index($from = 'tablegen') {
 		$this->set('title','DEBrowser');
-		if($from == 'index'){
-			$this->set('jsonobject','/?title=no');
-		}else if(isset($_SESSION['debrowser'])){
-			$this->set('jsonobject', '/?jsonobject='.urlencode($_SESSION['debrowser']).'&title=no');
-		}else if ($_SESSION['debrowser'] == ''){
-			$this->set('jsonobject','/?title=no');
+		if(isset($_SESSION['debrowser'])){
+			$this->set('jsonobject', '/?jsonobject='.urlencode($_SESSION['debrowser']).'&username='.$_SESSION['user'] );
 		}else{
-			$this->set('jsonobject','/?title=no');
+			$this->set('jsonobject','/?jsonobject=yes&username='.$_SESSION['user'] );
 		}
 	}
 	
